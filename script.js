5788afbc5bf7720940d0c0405d4c9d312de5567c
Phonique <41431476+marlybone@users.noreply.github.com>
2022-12-13 22:23:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var myLngLat = {lat: 51.4931, lng: -0.118092};
+
+ var mapStyle = [{
+          'featureType': 'all',
+          'elementType': 'all',
+          'stylers': [{'visibility': 'off'}]
+        }, {
+          'featureType': 'landscape',
+          'elementType': 'geometry',
+          'stylers': [{'visibility': 'on'}, {'color': '#fcfcfc'}]
+        }, {
+          'featureType': 'water',
+          'elementType': 'labels',
+          'stylers': [{'visibility': 'off'}]
+        }, {
+          'featureType': 'water',
+          'elementType': 'geometry',
+          'stylers': [{'visibility': 'on'}, {'hue': '#5f94ff'}, {'lightness': 60}]
+        }];
+
+
+
